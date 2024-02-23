196b3412f17abf86989186cf8ff1c60191d54c87
jiushizhe <233232874@qq.com>
2024-02-23 20:55:14 +0800

    Update AV集中营.py

--- H包/py/AV集中营.py
+++ H包/py/AV集中营.py
@@ -8,7 +8,7 @@ from base.spider import Spider
 import random
 
 sys.path.append('..')
-xurl = "https://jzy178.top"
+xurl = "https://jzy182.top"
 headerx = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36'
 }
